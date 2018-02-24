9684f31919d1d657cf57a17b3ff23cf3eeb56c33
Shadizzle <mail@shayden.me>
2018-02-25 01:08:29 +0930

    PlayerCard first version is done

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -5,11 +5,19 @@ class PlayerCard extends Component {
   render() {
     return (
       <div className="PlayerCard">
-        <div className="PlayerCard-mugshot">
-          
+        <div className="PlayerCard-banner">
+          <img className="PlayerCard-clubBadge"
+               src="/assets/Swans-rev.svg"
+               alt="The club's badge" />
+          <img className="PlayerCard-photo"
+               src="/assets/240399.png"
+               alt="A waist-up shot of player" />
+          <span className="PlayerCard-guernseyNumber">23</span>
         </div>
         <div className="PlayerCard-description">
-          <h2 className="PlayerCard-name">Lance <strong>Franklin</strong></h2>
+          <h2 className="PlayerCard-name">
+            Lance <span className="PlayerCard-lastName">Franklin</span>
+          </h2>
           <h3 className="PlayerCard-club">Sydney Swans</h3>
         </div>
       </div>
